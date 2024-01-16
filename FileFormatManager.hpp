fac04eb272cf89452da633ef0b91a46f19cb7b2d
MoXuToS <moxutosic@gmail.com>
2024-01-16 17:05:13 +0300

    Main

--- FileFormatManager.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<iostream>
-
-class FileFormatManager {
-public:
-    FileFormatManager();
-    void register_file_format(const std::string& file_format, const std::string& application);
-    void open_file(const std::string& file_path);
-};
