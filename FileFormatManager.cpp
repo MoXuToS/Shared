05a2383993e5630fdbb71d551d5d84d5a804e11b
moxutos <moxutosic@gmail.com>
2024-01-19 07:46:01 +0300

    Framework error fixed

--- FileFormatManager.cpp
+++ FileFormatManager.cpp
@@ -8,27 +8,16 @@ FileFormatManager::FileFormatManager ()
     setenv("PYTHONPATH", ".", 11);
     Py_Initialize();
     boost::python::object main_module = boost::python::import("Manager");
-    boost::python::object manager = main_module.attr("FileFormatManager");
-    manager.attr("FileFormatManager")();
+    boost::python::object manager = main_module.attr("FileFormatManager")();
 }
 
 void FileFormatManager::register_file_format(const std::string& file_format, const std::string& application)
 {
-    // Загрузка модуля Python
-    setenv("PYTHONPATH", ".", 1);
-    Py_Initialize();
-    boost::python::object main_module = boost::python::import("Manager");
-    boost::python::object manager = main_module.attr("FileFormatManager");
     manager.attr("register_file_format")(file_format, application);
 }
 
 void FileFormatManager::open_file(const std::string& file_path)
 {
-    // Загрузка модуля Python
-    setenv("PYTHONPATH", ".", 1);
-    Py_Initialize();
-    boost::python::object main_module = boost::python::import("Manager");
-    boost::python::object manager = main_module.attr("FileFormatManager");
     manager.attr("open_file")(file_path);
 }
 
