981f8770e93f17c5e6720bad9b15d57b0a516c06
moxutos <moxutosic@gmail.com>
2024-01-24 22:43:15 +0300

    main

--- FileFormatManager.hpp
+++ FileFormatManager.hpp
@@ -5,7 +5,7 @@
 class FileFormatManager 
 {
 public:
-    FileFormatManager()
+    FileFormatManager();
     void register_file_format(const std::string& file_format, const std::string& application);
     void open_file(const std::string& file_path);
 private:
