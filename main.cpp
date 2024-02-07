f4df4866e2d3b8241b797a1752b8d60ea49d565e
moxutos <moxutosic@gmail.com>
2024-02-08 00:01:04 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-
+#include <iostream>
+#include "FileFormatManager.hpp"
 using namespace std;
 
 int main()
 {
-    
+    Manager* FileManager;
+    FileManager->RegisterFormats(".txt", "Notebook");
+    FileManager->OpenFile("hehe.txt");
     return 0;
 }
