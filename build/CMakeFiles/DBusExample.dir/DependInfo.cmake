f82b5f2d6ac13d2fecb59f38f82bcacd1ae88f06
moxutos <moxutosic@gmail.com>
2024-02-08 12:32:45 +0300

    CMakeBuild

---
+++ build/CMakeFiles/DBusExample.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/git/Sharing-files/FileFormatManager.cpp" "CMakeFiles/DBusExample.dir/FileFormatManager.cpp.o" "gcc" "CMakeFiles/DBusExample.dir/FileFormatManager.cpp.o.d"
+  "/home/vboxuser/git/Sharing-files/main.cpp" "CMakeFiles/DBusExample.dir/main.cpp.o" "gcc" "CMakeFiles/DBusExample.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/DBusExample.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DBusExample.
+# This may be replaced when dependencies are built.
